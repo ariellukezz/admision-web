608a859ca3580fa962184c4164112894973fa9c3
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2025-09-09 15:39:32 -0500

    Update inscripcionExport.php

--- app/Exports/inscripcionExport.php
+++ app/Exports/inscripcionExport.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Exports;
-
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
