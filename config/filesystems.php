ffb25d0e981aef23af4afb06d38275fcb20483c8
Ariel Luque <40122258+ariellukezz@users.noreply.github.com>
2026-05-26 16:45:00 -0500

    actualizacion de dependencias y configuracion

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,12 @@ return [
             'throw' => false,
         ],
 
+        'documentos' => [
+            'driver' => 'local',
+            'root' => storage_path('app/documentos'),
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
