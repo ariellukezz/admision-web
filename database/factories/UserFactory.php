da917d1947702f39b7c7b0ec21a0f990985a5311
Ariel Luque <40122258+ariellukezz@users.noreply.github.com>
2026-06-24 10:00:00 -0500

    chore: actualizar UserFactory y permission tables

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,36 +3,34 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'dni'      => $this->faker->numerify('########'),
+            'name'     => $this->faker->firstName(),
+            'paterno'  => $this->faker->lastName(),
+            'materno'  => $this->faker->lastName(),
+            'email'    => $this->faker->unique()->safeEmail(),
+            'celular'  => $this->faker->numerify('9########'),
+            'password' => Hash::make('password'),
+            'id_rol'   => 2,
+            'id_proceso' => 1,
+            'estado'   => 1,
+            'programas' => null,
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function admin(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(fn (array $attributes) => ['id_rol' => 1]);
+    }
+
+    public function revisor(): static
+    {
+        return $this->state(fn (array $attributes) => ['id_rol' => 2]);
     }
 }
