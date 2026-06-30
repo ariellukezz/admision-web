d9cfff29fb30108c4344d6cae0fe4afcb63d6331
Ariel Luque <40122258+ariellukezz@users.noreply.github.com>
2026-06-30 16:30:00 -0500

    build: actualizacion de assets compilados

--- public/build/assets/auto-be52a683.js
+++
@@ -1,1 +0,0 @@
-import{C as r,r as e}from"./chart-47581b73.js";r.register(...e);
---
+++ public/build/assets/constancia-be2fbda4.js
@@ -0,0 +1,1 @@
+const s="/build/assets/amarillo-4d9aaa33.jpg",a="/build/assets/blanco-faf0a115.jpg",t="/build/assets/constancia-1db23441.jpg";export{s as _,a,t as b};
