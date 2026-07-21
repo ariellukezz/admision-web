e0191500184ad981a6cd3ac01451e505e5024178
Ariel Luque <40122258+ariellukezz@users.noreply.github.com>
2026-07-21 07:02:32 -0500

    actualización de NavigationCalificador

--- resources/js/Layouts/NavigationCalificador.vue
+++ resources/js/Layouts/NavigationCalificador.vue
@@ -44,6 +44,23 @@
             </NavLink>
           </li>
 
+          <li class="nav-gradient-item" :class="{ 'nav-gradient-active': route().current('calificar-lecturas') }">
+            <NavLink :href="route('calificar-lecturas')" :active="route().current('calificar-lecturas')" class="nav-gradient-link">
+              <div class="nav-content-wrapper">
+                <div class="nav-gradient-icon">
+                  <div class="icon-bg"></div>
+                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
+                    <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M7 10L12 15L17 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 15V3" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                </div>
+                <span class="nav-gradient-text">Carga de Lecturas</span>
+              </div>
+              <div class="nav-highlight"></div>
+            </NavLink>
+          </li>
+
           <li class="nav-gradient-item" :class="{ 'nav-gradient-active': route().current('simulacro-inscritos') }">
             <NavLink :href="route('simulacro-inscritos')" :active="route().current('simulacro-inscritos')" class="nav-gradient-link">
               <div class="nav-content-wrapper">
