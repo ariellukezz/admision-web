a465d36451ca27147701ffff8e00e72ea30fa7f0
Ariel Luque <40122258+ariellukezz@users.noreply.github.com>
2026-08-10 09:47:43 -0500

    ajustes de constancia de ingreso

--- resources/views/ingreso/datosbiometricos.blade.php
+++ resources/views/ingreso/datosbiometricos.blade.php
@@ -37,11 +37,11 @@
     
         <p style="line-height: 1.5rem;">
             La Dirección de Admisión de la Universidad Nacional del Altiplano de Puno, en cumplimiento pleno del 
-            Reglamento General de Admisión 2026, <label style="font-weight:bold;">HACE CONSTAR </label> 
+            Reglamento General de Admisión 2026-II, <label style="font-weight:bold;">HACE CONSTAR </label> 
             que <label style="font-weight:bold;">{{$data->paterno}} {{$data->materno}} {{$data->nombres}}</label>, identificado (a) con 
             DNI N° <label style="font-weight:bold;">{{$data->dni}}</label>, es 
             <label style="font-weight:bold;">INGRESANTE APTO</label> al programa de estudios de <label style="font-weight:bold;">{{$data->programa}}</label>,
-            con duracion total de <label style="font-weight:bold;">{{$data->duracion_programa}}</label>, 
+            con duración total de <label style="font-weight:bold;">{{$data->duracion_programa}}</label>, 
             bajo la modalidad <label style="font-weight:bold;">{{$data->modalidad}}</label>, El ingresante está en el puesto <label style="font-weight:bold;">{{ $data->puesto }}</label> y obtuvo el puntaje de
             <label style="font-weight:bold;">{{ $data->puntaje }} </label>puntos el {{ $date }}. El (la) estudiante en mención,
             queda expedito(a) con código <label style="font-weight:bold;"> {{$data->cod_ingreso}} </label> para matricularse en el referido programa de estudios semestre 2026-II, siendo la modalidad de estudio 
